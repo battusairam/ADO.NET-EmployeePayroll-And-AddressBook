b033640414a069db632c645939b65c8672ae97c0
“battusairam” <battusairam6@gmail.com>
2023-06-24 23:06:00 +0530

    [SAIRAM] Add: ADO.net Employee Payroll Problem

---
+++ ADO.NET-Employee Payroll/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace ADO.NET_EmployeePayroll_And_AddressBook
+{
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Employee payroll Connectivity Database with .Net applications using Ado.Net!");
+
+                PayrollServices obj = new PayrollServices();
+                // obj.CreateDataBase();
+                // obj.CreateTable();
+                // obj.InsertData();
+                obj.RetrieveData();
+            }
+        }
+    }
